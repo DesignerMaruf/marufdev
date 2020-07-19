a8990d656c2337077df15e73ce766e8622272c07
Md. Maruf Ahmed <60195769+DesignerMaruf@users.noreply.github.com>
2020-07-19 08:39:34 +0600

    2nd

--- js/script.js
+++ js/script.js
@@ -2,30 +2,9 @@
 $(document).on('click', 'ul li', function () {
     $(this).addClass('active').siblings().removeClass('active')
 });
-
-
 // MIXITUP
-
 var mixer = mixitup('.portfolio-work-container', {
     animation: {
         duration: 400
     }
 });
-
-//$('.someBlock').preloader({
-//
-//    // loading text
-//    text: 'Loading...',
-//
-//    // from 0 to 100 
-//    percent: '99.5',
-//
-//    // duration in ms
-//    duration: '300',
-//
-//    // z-index property
-//    zIndex: '999999999999999999',
-//
-//    // sets relative position to preloader's parent
-//    setRelative: false,
-//});
